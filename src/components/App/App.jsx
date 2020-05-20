d2eeb53c16cb47cbf4fb716835da59ebf566cd56
Cody Brown <cody6lefever@gmail.com>
2020-05-20 10:36:16 -0700

    initial setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
+// import './App.css';
+// import Home from './Home.js';
+// import AboutUs from './AboutUs.js';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+
+
+        <div>
+          <Route exact path ='/' component={Home}/>
+          <Route exact path ='/about-us' component={AboutUs}/>
+        </div>
+        
+        
+      </Router>
+    );
+  }
 }
-  
