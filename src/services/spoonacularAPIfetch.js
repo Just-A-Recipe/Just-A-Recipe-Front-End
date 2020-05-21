f94d217912024bded5e4cf05037ca001c5dbc966
james eserjose <james.re@yahoo.com>
2020-05-21 16:36:14 -0700

    services

--- src/services/spoonacularAPIfetch.js
+++ src/services/spoonacularAPIfetch.js
@@ -1,6 +1,22 @@
+// require('dotenv').config();
+
+// export const fetchRecipe = () => {
+//   return fetch(`${process.env.REACT_APP_API_URL}/api/v1/recipes`)
+//     .then(res => res.json());
+// };
+
+const request = require('superagent');
 require('dotenv').config();
 
-export const fetchRecipe = () => {
-  return fetch(`${process.env.REACT_APP_API_URL}/api/v1/recipes`)
-    .then(res => res.json());
-};
+function fetchSimilarRecipes() {
+  const id =  595654;
+  return request
+    .get(`https://api.spoonacular.com/recipes/${id}/similar?${process.env.API_KEY}`)
+    .then(res => res);
+}
+
+fetchSimilarRecipes().then(res => console.log(res.body));
+
+// module.exports = {
+//   fetchSimilarRecipes
+// };
