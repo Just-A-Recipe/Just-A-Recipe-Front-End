2f7510551d85d9fb5a2f627900fd52ce11686f82
surelis segarra <sdarianne34@gmail.com>
2020-05-22 10:58:03 -0700

    not done yet

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router,
-  Link,
-  Route,
-  Switch
-} from 'react-router-dom';
+// import { BrowserRouter as Router,
+//   Link,
+//   Route,
+//   Switch
+// } from 'react-router-dom';
 // import './App.css';
 // import Home from './Home.js';
 // import AboutUs from './AboutUs.js';
@@ -13,19 +13,19 @@ import { BrowserRouter as Router,
 export default class App extends Component {
   render() {
     return (
-      <Router>
+    // <Router>
 
 
+      <div>
+        {/* <Route exact path ='/' component={Home}/> */}
         <div>
-          {/* <Route exact path ='/' component={Home}/> */}
-          <div>
-            <img src="https://media.tenor.com/images/213a4c8c6fb278cf2fb10bcc0da1b68d/tenor.gif"></img>
-          </div>
-          {/* <Route exact path ='/about-us' component={AboutUs}/> */}
+          <img src="https://media.tenor.com/images/213a4c8c6fb278cf2fb10bcc0da1b68d/tenor.gif"></img>
         </div>
+        {/* <Route exact path ='/about-us' component={AboutUs}/> */}
+      </div>
         
         
-      </Router>
+    // </Router>
     );
   }
 }
--- src/services/spoonacularAPIfetch.js
+++ src/services/spoonacularAPIfetch.js
@@ -1,22 +1,4 @@
-// require('dotenv').config();
-
-// export const fetchRecipe = () => {
-//   return fetch(`${process.env.REACT_APP_API_URL}/api/v1/recipes`)
-//     .then(res => res.json());
-// };
-
-const request = require('superagent');
-require('dotenv').config();
-
-function fetchSimilarRecipes() {
-  const id =  595654;
-  return request
-    .get(`https://api.spoonacular.com/recipes/${id}/similar?${process.env.API_KEY}`)
-    .then(res => res);
-}
-
-fetchSimilarRecipes().then(res => console.log(res.body));
-
-// module.exports = {
-//   fetchSimilarRecipes
-// };
+export const fetchRecipes = searchQuery => {
+  return fetch(`https://just-a-recipe.herokuapp.com/api/v1/recipes?searcQuery=${searchQuery}`)
+    .then(res => res.json());
+};
