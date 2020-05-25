3e48809d1cefa21dcee40013b9837dd13cd70f69
james eserjose <james.re@yahoo.com>
2020-05-25 09:46:51 -0700

    working

--- src/containers/RecipeList/RecipeViewer.jsx
+++ src/containers/RecipeList/RecipeViewer.jsx
@@ -1,57 +1,29 @@
-// import React, { useState, useEffect,  } from 'react';
-// import RecipeList from '../../components/RecipeList/RecipeList';
-// import { fetchRecipes } from '../../services/spoonacularAPIFetch';
-import SearchBar from '../../components/Search/SearchBar';
+import React, { useState, useEffect } from 'react';
+import RecipeList from '../../components/RecipeList/RecipeList';
+import { fetchRecipes } from '../../services/spoonacularAPIFetch';
 
-// const RecipeViewer = () => {
-//   const [page, setPage] = useState(1);
-//   const [recipes, setRecipes] = useState(['']);
-//   const searchQuery = '';
+const RecipeViewer = () => {
+  const [page, setPage] = useState(1);
+  const [recipes, setRecipes] = useState(['']);
+  const searchQuery = '';
   
-//   // useEffect(() => {
-//   //   fetchRecipes(searchQuery)
-//   //     .then(fetchRecipes => setRecipes(fetchRecipes));
-//   //   console.log(searchQuery, '|||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+  useEffect(() => {
+    fetchRecipes(searchQuery)
+      .then(fetchRecipes => setRecipes(fetchRecipes));
+    console.log(searchQuery, '|||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
       
-//   // }, []);
-//  const handleChange = ({ target }) => {}
-//   const decrement = () => setPage(prevPage => prevPage - 1);
-//   const increment = () => setPage(prevPage => prevPage + 1);
+  }, []);
+
+  const decrement = () => setPage(prevPage => prevPage - 1);
+  const increment = () => setPage(prevPage => prevPage + 1);
   
-//   return (
-//     <>
-//       <button onClick={() => decrement(-1)} disabled={page === 1}>&lt;</button>
-//       <button onClick={() => increment(1)} disabled={recipes.length < 10}>&gt;</button>
-//       <RecipeList recipeList={recipes} />
-//     </>
-//   );
-// };
+  return (
+    <>
+      <button onClick={() => decrement(-1)} disabled={page === 1}>&lt;</button>
+      <button onClick={() => increment(1)} disabled={recipes.length < 10}>&gt;</button>
+      <RecipeList recipeList={recipes} />
+    </>
+  );
+};
 
-// export default RecipeViewer;
-
-import React, { Component } from 'react';
-
-export default class RecipeViewer extends Component {
-  state = {
-    searchText: ','
-  }
-
-  handleChange = ({ target }) => {
-    this.setState({ searchText: target.value });
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.setState(state => ({ searchText: '' }));
-  }
-
-  render() {
-    const { searchText } = this.state;
-
-    return (
-      <div>
-        <SearchBar text={searchText} onChange={this.handleChange} onSubmit={this.handleSubmit}  />
-      </div>
-    )
-  }
-}
+export default RecipeViewer;
