7d45c4e8508c5373580857468dafd214309828d4
Cody Brown <cody6lefever@gmail.com>
2020-05-26 14:21:13 -0700

    basic header and footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import styles from './Header.css';
+import styles from './Footer.css';
 
 
 export default class Header extends Component {
   render() {
     return (
-      <div className={styles.headerDiv}>
-        <h1>Just a Recipe</h1>
+      <div className={styles.footerDiv}>
+        <h5>Contact Us</h5>
+        <h5>Alchemy Code Lab</h5>
       </div>
     );
   }
