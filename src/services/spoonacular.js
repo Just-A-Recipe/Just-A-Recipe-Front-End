93787265e512cc988ba65f4d825b468ef916fd35
james eserjose <james.re@yahoo.com>
2020-05-27 17:13:42 -0700

    gluten free filter working

--- src/services/spoonacular.js
+++ src/services/spoonacular.js
@@ -12,7 +12,6 @@ export const fetchRecipes = (searchQuery, offset) => {
 
 export const fetchRecipe = (id) => {
   return fetch(`https://just-a-recipe.herokuapp.com/api/v1/recipes/${id}`)
-  
     .then(res => res.json())
     .then(json => ({
       title: json.title,
@@ -25,9 +24,22 @@ export const fetchRecipe = (id) => {
 };
 
 export const fetchRecipeImage = image => {
-  return fetch (`https://just-a-recipe.herokuapp.com/ap/v1/recipeimages/${id}-312x231.jpg`)
+  return fetch(`https://just-a-recipe.herokuapp.com/ap/v1/recipeimages/${id}-312x231.jpg`)
     .then(res => res.json(image))
     .then(json => ({
       image: json.image
     }));
 };
+
+export const fetchGlutenFree = (searchQuery, offset) => {
+  return fetch(`https://just-a-recipe.herokuapp.com/api/v1/recipes/?searchQuery=${searchQuery}&offset=${offset}&intolerances=Gluten`)
+    .then(res => res.json())
+    .then(json => json.map(({ id, title, image, extendedIngredients, analyzedInstructions }) => ({
+      id,
+      title,
+      image,
+      extendedIngredients,
+      analyzedInstructions
+    })));
+};
+
